14c8a287686ba1547e6db63b61c8b85d86d62559
aartaka <aartaka@protonmail.com>
2022-08-29 19:58:07 +0400

    gopher: Add more logging.

--- gopher.lisp
+++ gopher.lisp
@@ -98,6 +98,12 @@
                                :backlog (hunchentoot:acceptor-listen-backlog acceptor)))
   (values))
 
+(defmethod hunchentoot:accept-connections :before ((acceptor gopher-acceptor))
+  (hunchentoot:log-message* :info "Accepting gopher connection."))
+
+(defmethod hunchentoot:process-connection :before ((acceptor gopher-acceptor))
+  (hunchentoot:log-message* :info "Starting gopher request processing."))
+
 ;; Fix selectors so that files are serveable as submenus/textfiles.
 (defmethod hunchentoot:process-connection ((acceptor gopher-acceptor) socket)
   (flet ((read-path ()
