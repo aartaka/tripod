576db41e9398bdace1fea5cac69ebc196e18d6ae
aartaka <aartaka@protonmail.com>
2022-09-02 11:11:50 +0400

    gopher: Properly add blanks after lists.

--- gopher.lisp
+++ gopher.lisp
@@ -78,15 +78,16 @@ ALWAYS returns a list, even if there's just one string in it."
    (mkline 'cl-gopher:info-message "")))
 
 (defmethod tripod->backend ((node items) (backend (eql +gopher+)) &key)
-  (alexandria:mappend
-   (lambda (e)
-     (let ((terminal-lines (split-for-terminal e 77)))
-       (cons (mkline 'cl-gopher:info-message (uiop:strcat "- " (first terminal-lines)))
-             (mapcar (lambda (l)
-                       (mkline 'cl-gopher:info-message (uiop:strcat "  " l)))
-                     (rest terminal-lines)))))
-   (append (elements node)
-           (list ""))))
+  (append
+   (alexandria:mappend
+    (lambda (e)
+      (let ((terminal-lines (split-for-terminal e 77)))
+        (cons (mkline 'cl-gopher:info-message (uiop:strcat "- " (first terminal-lines)))
+              (mapcar (lambda (l)
+                        (mkline 'cl-gopher:info-message (uiop:strcat "  " l)))
+                      (rest terminal-lines)))))
+    (elements node))
+   (mkline 'cl-gopher:info-message "")))
 
 (defmethod tripod->backend ((node link) (backend (eql +gopher+)) &key)
   (let ((mime (mimes:mime (quri:uri-path (href node))))
