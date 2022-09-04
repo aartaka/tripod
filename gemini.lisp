b6f3f6a8bd149fd379f3bee1ff25d88ca3cffc23
aartaka <aartaka@protonmail.com>
2022-09-04 13:26:14 +0400

    gemini: Log characters as characters.

--- gemini.lisp
+++ gemini.lisp
@@ -121,7 +121,7 @@
                               until (and (= char (char-code #\newline))
                                          prev
                                          (= prev (char-code #\return)))
-                              do (hunchentoot:log-message* :info "Read a character: ~s~%" char)
+                              do (hunchentoot:log-message* :info "Read a character: ~s ~s" char (code-char char))
                               do (vector-push-extend char vec)
                               finally (let ((path (progn
                                                     (vector-pop vec)
